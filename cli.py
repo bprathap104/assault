d22b14bb8c400e938acad3b25c5c8b4c074db578
Saravanan <bprathap104@gmail.com>
2020-09-20 02:39:27 +0000

    Reading from command line - click

--- cli.py
+++ cli.py
@@ -2,8 +2,15 @@ import click
 
 
 @click.command()
-def cli():
-    pass
+@click.option("--requests", "-r", default=500, help="Number of requests")
+@click.option("--concurrency", "-c", default=1, help="Number of cuncurrent requests")
+@click.option("--json-file", "-j", default=None, help="Path to output json file")
+@click.argument("url")
+def cli(requests, concurrency, json_file, url):
+    print(f"Requests: {requests}")
+    print(f"Concurrency: {concurrency}")
+    print(f"JSON file: {json_file}")
+    print(f"URL: {url}")
 
 
 if __name__ == "__main__":
